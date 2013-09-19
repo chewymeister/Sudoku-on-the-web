d2d92f7f4ffb362464fc643fafc0250fb6f2f4c1
Jonathan Tsang <chewymeister88@googlemail.com>
2013-09-19 22:11:03 +0100

    Application functional. Colour class not working

--- sudoku.rb
+++ sudoku.rb
@@ -1,14 +1,12 @@
 require 'sinatra'
+# require 'sinatra/partial'
+# require 'rack-flash'
 require_relative './lib/cell'
 require_relative './lib/grid'
 require_relative './lib/box'
 
 enable :sessions
 
-get '/' do
-  @current_solution = random_sudoku
-  erb :index
-end
 
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9,0)
@@ -19,11 +17,94 @@ def random_sudoku
 end
 
 def puzzle sudoku
-  random_index = (0..80).sample(40)
-  random_index.map! do |index|
-    sudoku[index] = '0'
+  sudoku_puzzle = sudoku.clone
+  random_index = (0..80).to_a.sample(40)
+  random_index.each do |index|
+    sudoku_puzzle[index] = '0'
+  end
+  sudoku_puzzle
+end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]
+end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+end
+
+def box_order_to_row_order cells
+   boxes = cells.each_slice(9).to_a
+   (0..8).to_a.inject([]) do |memo,i|
+   first_box_index = i/3*3
+   three_boxes = boxes[first_box_index, 3]
+   three_rows_of_three = three_boxes.map do |box|
+    row_number_in_a_box = i%3
+    first_cell_in_the_row_index = row_number_in_a_box * 3
+    box[first_cell_in_the_row_index,3]
+    end
+    memo += three_rows_of_three.flatten
   end
 end
 
-  sudoku
+helpers do
+
+  def colour_class solution_to_check, puzzle_value, current_solution_value, solution_value
+    must_be_guessed = puzzle_value == "0"
+    tried_to_guess = current_solution_value.to_i != 0
+    guessed_incorrectly = current_solution_value != solution_value
+
+    if solution_to_check &&
+      must_be_guessed &&
+      tried_to_guess &&
+      guessed_incorrectly
+      'incorrect'
+    elsif !must_be_guessed
+      'value-provided'
+    end
+  end
+
+  def cell_value value
+    value.to_i == 0 ? '' : value
+  end
+
 end
+
+#===========================================================================================
+#                                       SINATRA COMMANDS
+#===========================================================================================
+
+get '/' do
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
+  erb :index
+end
+
+get '/solution' do
+  @puzzle = session[:solution]
+  erb :index
+end
+
+post '/' do
+  cells = box_order_to_row_order(params["cell"])
+  session[:current_solution] = cells.map{|value| value.to_i}.join
+  print "This is after post                #{session[:current_solution]} \n"
+  session[:check_solution] = true
+  redirect to ("/")
+end
+
+get '/reset' do
+  session.delete(:current_solution)
+  print "This is after get                #{session[:current_solution]} \n"
+  redirect to ("/")
+end
+
+
