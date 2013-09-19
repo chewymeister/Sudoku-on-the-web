325d53feba3c2285dd7d48c2507feebf176a3b66
Jonathan Tsang <chewymeister88@googlemail.com>
2013-09-19 11:06:57 +0100

    added puzzle sudoku method

--- sudoku.rb
+++ sudoku.rb
@@ -3,6 +3,8 @@ require_relative './lib/cell'
 require_relative './lib/grid'
 require_relative './lib/box'
 
+enable :sessions
+
 get '/' do
   @current_solution = random_sudoku
   erb :index
@@ -14,4 +16,14 @@ def random_sudoku
   sudoku.set_board
   sudoku.solve_board!
   sudoku.provide_puzzle
+end
+
+def puzzle sudoku
+  random_index = (0..80).sample(40)
+  random_index.map! do |index|
+    sudoku[index] = '0'
+  end
+end
+
+  sudoku
 end
